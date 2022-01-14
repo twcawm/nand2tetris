fc64dc5ec4d3fc0dfda0da12807ac4c601808e36
twcawm <twcawm@gmail.com>
2022-01-14 16:11:33 -0500

    implement all methods of JackTokenizer, verify that token list looks ok

--- projects/10/jackTokenizer.py
+++ projects/10/jackTokenizer.py
@@ -1,21 +1,26 @@
+import re
+
 class JackTokenizer:
 
-  l_keywords = {"class", "constructor", "function", "method", "field", "static", "var", 
+  l_keywords = ["class", "constructor", "function", "method", "field", "static", "var", 
               "int", "char", "boolean", "void", "true", "false", "null", "this",
-              "let", "do", "if", "else", "while", "return"}
-  l_symbols = {"{","}","(",")","[","]",".",",",";","+","-","*","/","&","|","<",">","=","-"}
+              "let", "do", "if", "else", "while", "return"]
+  l_symbols = ["{","}","(",")","[","]",".",",",";","+","-","*","/","&","|","<",">","=","-"]
 
-  re_KEYWORD = '(?<!\w)('
-  re_KEYWORD = re_KEYWORD + "|".join(l_keywords)
-  re_KEYWORD = re_KEYWORD + ')(?!\w)'
+  re_KEYWORD = '(?<!\w)' + '(?!\w)|(?<!\w)'.join(l_keywords)+'(?!\w)'
   # '(?!\w)|' means "do not match \w (unicode word characters) after this".  \w means [a-zA-Z0-9_] basically.
   # and '(?<!\w)' does the same thing for "behind this".  basically, negative lookahead and lookbehind.
-  '''
-  re_SYMBOL 
-  re_IDENTIFIER
-  re_INT_CONST
-  re_STRING_CONST
-  '''
+  # note: doing this with '(?<!\w)(class|constructor|...)(?!\w)' DID NOT WORK when combined with other regex,
+  #   but worked on its own.  pretty confusing.
+  re_SYMBOL = "[" + re.escape("|".join(l_symbols)) + "]" #re.escape ensures the characters like { or . (characters with special meaning in regex) are escaped to mean literally those characters
+  re_IDENTIFIER = r"[\w]+" #+ means "at least 1".  \w means word character.  [] means a set.  I'm not 100% sure if the [] is needed here, but this works.
+  re_INT_CONST = r"[0-9]+" # similar to re_IDENT.  \d matches things like... Eastern Arabic etc.
+  re_STRING_CONST = r'"[^"\n]*"' #starts with ".  [^...] denotes COMPLEMENT of a group. so match any number of anything except newlines and other ", then end with ".  
+
+  #re_lex_element = "|".join([re_KEYWORD,re_SYMBOL,re_IDENTIFIER,re_INT_CONST,re_STRING_CONST])
+  re_lex_element = "|".join([re_KEYWORD,re_SYMBOL,re_INT_CONST,re_STRING_CONST,re_IDENTIFIER])
+  print("re_lex_element: " + re_lex_element)
+  compiled_lex_element = re.compile(re_lex_element) #this is an attempt to match all lexical elements of the Jack language
 
   def __init__(self, filename):
     f_jack = open(filename,'r')
@@ -23,8 +28,75 @@ class JackTokenizer:
     f_jack.close()
    
     self.remove_comments()
+    self.all_lex_elements = self.compiled_lex_element.findall(self.txt_jack) #list of all lexical elements
+    self.tokens = self.make_token_list(self.all_lex_elements) #list of tuples of lexelement type, value (lexement)
+ 
+    self.current_index = -1 #we need to all advance() to get to the first element of the tokens list.
+    self.current_token = None
 
+  def hasMoreTokens(self):
+    return self.current_index < (len(self.tokens) - 1)
 
+  def advance(self):
+    if(not hasMoreTokens):
+      print("error: advance() called without any more tokens left")
+    else:
+      self.current_index = self.current_index + 1
+      self.current_token = self.tokens[self.current_index]
+
+  def tokenType(self): #returns which type of lexical element it is
+    return self.current_token[0] #we already set up self.tokens to store this info
+
+  def keyWord(self): #returns which keyword it is
+    if(self.tokenType() == "KEYWORD"):
+      return self.current_token[1] #essentially already stored
+    else:
+      print("error: keyWord() called when tokenType() is not KEYWORD")
+
+  def symbol(self):
+    if(self.tokenType() == "SYMBOL"):
+      return self.current_token[1]
+    else:
+      print("error: symbol() called when tokenType() is not SYMBOL")
+
+  def identifier(self): 
+    if(self.tokenType() == "IDENTIFIER"):
+      return self.current_token[1]
+    else:
+      print("error: identifier() called when tokenType() is not IDENTIFIER")
+
+  def intVal(self): 
+    if(self.tokenType() == "INT_CONST"):
+      return int(self.current_token[1])
+    else:
+      print("error: intVal() called when tokenType() is not INT_CONST")
+
+  def stringVal(self):
+    if(self.tokenType() == "STRING_CONST"):
+      return int(self.current_token[1])
+    else:
+      print("error: stringVal() called when tokenType() is not STRING_CONST")
+
+  def make_token_list(self, l_lex_elements):
+    return [(self.lex2token(lex_element), lex_element) for lex_element in l_lex_elements]
+
+  def lex2token(self, lex_element): #lex_element: one element of the list returned by findall on compiled_lex_element
+    if(re.match(self.re_KEYWORD, lex_element) ):
+      return "KEYWORD"
+    elif(re.match(self.re_SYMBOL, lex_element) ):
+      return "SYMBOL"
+    elif(re.match(self.re_INT_CONST, lex_element) ):
+      return "INT_CONST"
+    elif(re.match(self.re_STRING_CONST, lex_element) ):
+      return "STRING_CONST"
+    elif(re.match(self.re_IDENTIFIER, lex_element) ):
+      return "IDENTIFIER"
+    else:
+      print("error in lex2token in lexer: lexical element not recognized")
+
+  
+   
+    
 
   def remove_comments(self):
     #parse through the entire self.txt_jack
--- projects/10/testTokenizer.py
+++ projects/10/testTokenizer.py
@@ -3,3 +3,5 @@ import jackTokenizer
 tok = jackTokenizer.JackTokenizer("Square/Main.jack")
 
 print(tok.txt_jack)
+print(tok.all_lex_elements)
+print(tok.tokens)
